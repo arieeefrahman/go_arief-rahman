b149cc4abee10ec868556d4dbd35da665603edce
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2022-09-29 03:28:57 +0700

    app

---
+++ 18_ORM & Code Structure (MVC)/praktikum/problem-2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"prak-18-prob-2/database"
+	"prak-18-prob-2/routes"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/labstack/echo/v4"
+)
+
+
+func main() {
+	// initiliaze database
+	database.Init()
+
+	// create a new echo instance
+	e := echo.New()
+	
+	// Route to handler function
+	routes.InitRoutes(e)
+	
+	// start the server, and log if it fails
+	e.Logger.Fatal(e.Start(":8000"))
+}
