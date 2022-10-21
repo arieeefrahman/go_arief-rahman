ea565eefb8e12c5a31b44c2810b8d831cf551365
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2022-10-21 17:03:15 +0700

    (model): initial commit

---
+++ weekly-task-2/source-code/models/model.go
@@ -0,0 +1,18 @@
+package models
+
+import "gorm.io/gorm"
+
+type Product struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Description string `gorm:"type:text"  json:"description"`
+	Stock       int    `json:"stock"`
+	Price       int    `json:"price"`
+	CategoryID  int    `json:"category_id"`
+	Category    Category
+}
+
+type Category struct {
+	ID   int    `gorm:"primaryKey"`
+	Name string `gorm:"unique" json:"name"`
+}
